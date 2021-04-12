dc2091b0b853e9690dc42d52e250a2183a64ac01
arif286 <71484413+arif286@users.noreply.github.com>
2021-04-13 00:06:01 +0600

    doctor-portals

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDcQEDvaVHWhesWpiVOHG-bVCBP1-jxBbI",
+  authDomain: "dental-doctor-portals.firebaseapp.com",
+  projectId: "dental-doctor-portals",
+  storageBucket: "dental-doctor-portals.appspot.com",
+  messagingSenderId: "886192467913",
+  appId: "1:886192467913:web:8211ef3dd5e3971759c218",
+};
+export default firebaseConfig;
